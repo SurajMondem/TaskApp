f9ae8b6a0068358ce510eb30e65a7445ccd5f0ba
SurajMondem <surajmondem@gmail.com>
2020-04-07 15:29:55 -0400

    Pushing User Schema with Encryption

---
+++ Backend/models/user.js
@@ -0,0 +1,79 @@
+var mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
+
+
+var userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        maxlength: 32,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique:true
+    },
+    userinfo: {
+        type: String,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    salt: String,
+    username: {
+        type: String,
+        trim: true,
+        maxlength: 20,
+        // unique: true     TODO: DO MAKE CHANGES HERE
+    },
+    boards: {
+        type: Array,
+        default: []
+    },
+    role: {
+        type: Number,
+        default: 0
+    }
+});
+
+userSchema.virtual("password")
+    .set(function(password){
+        this._password = password;
+        this.salt = uuidv1();
+        this.password = this.securePassword(password);
+    })
+    .get(function(){
+        return this._password
+    })    
+
+userSchema.method = {
+
+    authenticate: function(plainpassword){
+        return this.securePassword(plainpassword) === this.password;
+    },
+
+    securePassword: function(plainpassword){
+        if(!password) return "";
+        try {
+            return crypto
+            .createHmac('sha256', this.salt)
+            .update(plainpassword)
+            .digest('hex');
+        } catch (error) {
+            return "";
+        }
+    }
+}
+
+
+module.exports = mongoose.model("User", userSchema);
