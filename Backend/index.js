710e53ace924e9ae6280834fba82246575878c62
SurajMondem <surajmondem@gmail.com>
2020-04-10 10:48:52 -0400

    Setup Database Connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
 const port = 4000;
+
+const databaseUrl = "mongodb://localhost:27017/Task";
+
+mongoose.connect(databaseUrl ,{
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+     useCreateIndex: true 
+    })
+    .then(() => {
+        console.log("DATABASE CONNECTED...");
+    });
 
 app.get('/', (request, response) => {
     return response.send('Hello world')
